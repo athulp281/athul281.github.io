47881b86024d2b536ecca039ebdde2120c276a84
athulp281 <athulpranoor@gmail.com>
2023-10-22 22:57:21 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -6,41 +6,51 @@ import FeatureBox from "./components/FeatureBox";
 import AboutBox from "./components/AboutBox";
 import ProjectBox from "./components/ProjectBox";
 import MyContextProvider from "./useContext/MyContextProvider";
+import { useEffect, useState } from "react";
+import Loader from "./components/Loader";
 // import { MyContextProvider } from "./useContext/MyContextProvider";
 
 function App() {
-    return (
+  const [showLoader, setShowLoader] = useState(true);
+  useEffect(() => {
+    // Hide the loader after 1 second
+    const loaderTimeout = setTimeout(() => {
+      setShowLoader(false);
+    }, 1500);
+
+    return () => {
+      clearTimeout(loaderTimeout); // Clear the timeout to prevent memory leaks
+    };
+  }, []);
+  return (
+    <>
+      {showLoader && <Loader />}
+      {!showLoader && (
         <MyContextProvider>
-            <Router>
-                <div>
-                    <Routes>
-                        <Route exact path="/" element={<Home />}></Route>
-                        <Route
-                            exact
-                            path="/contact"
-                            element={<Contact />}
-                        ></Route>
-                        <Route exact path="/head" element={<Header />}></Route>
-                        <Route
-                            exact
-                            path="/featurebox"
-                            element={<FeatureBox />}
-                        ></Route>
-                        <Route
-                            exact
-                            path="/aboutbox"
-                            element={<AboutBox />}
-                        ></Route>
-                        <Route
-                            exact
-                            path="/projectbox"
-                            element={<ProjectBox />}
-                        ></Route>
-                    </Routes>
-                </div>
-            </Router>
+          <Router>
+            <div>
+              <Routes>
+                <Route exact path="/" element={<Home />}></Route>
+                <Route exact path="/contact" element={<Contact />}></Route>
+                <Route exact path="/head" element={<Header />}></Route>
+                <Route
+                  exact
+                  path="/featurebox"
+                  element={<FeatureBox />}
+                ></Route>
+                <Route exact path="/aboutbox" element={<AboutBox />}></Route>
+                <Route
+                  exact
+                  path="/projectbox"
+                  element={<ProjectBox />}
+                ></Route>
+              </Routes>
+            </div>
+          </Router>
         </MyContextProvider>
-    );
+      )}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Loader.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import "../assets/Css/loader.css";
+import { CSSTransition } from "react-transition-group";
+
+export default function Loader() {
+  const [percentage, setPercentage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Update the percentage value
+      if (percentage < 100) {
+        setPercentage((prevPercentage) => prevPercentage + 1);
+      }
+    }, 10); // You can adjust the speed by changing the interval duration
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [percentage]);
+  return (
+    <CSSTransition
+      in={true} // Set to true initially
+      timeout={1000} // Match the animation duration
+      classNames="loader" // Use the animation class names
+      unmountOnExit
+    >
+      <div className="loader">
+        <div className="percentage-loader">
+          <div className="percentage-text">{percentage}%</div>
+          <div className="progress-bar">
+            <div className="progress" style={{ width: `${percentage}%` }}></div>
+          </div>
+        </div>
+      </div>
+    </CSSTransition>
+  );
+}
